bef2618f40d29b5a50eb994427d2564b048af73a
toprakcakar66 <39004667+toprakcakar66@users.noreply.github.com>
2018-05-12 15:50:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,13 +17,9 @@ bot.on("message", message => {
     if (message.content.toLowerCase() === "sa") {
         message.reply("**Aleyküm Selam!**")
     }
-    
-    if (message.content.toLowerCase() === "Toprak") {
-        message.reply("**Bir sorun mu oldu.Sorun varsa t!yardım yaz.")
-    }
 
-    if (msg.content === 'Toprak') {
-    msg.reply('pong');
+    if (msg.content.toLowerCase() === 'Toprak') {
+    msg.reply('Bir sorun mu oldu.Sorun varsa t!yardım yaz.');
     }
 
     
